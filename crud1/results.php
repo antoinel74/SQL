901368245450fc95858f05d0214a524191557dc3
antoinel74 <antoine.lansman@gmail.com>
2023-11-13 11:11:14 +0100

    crud1 done

--- crud1/results.php
+++ crud1/results.php
@@ -2,21 +2,40 @@
 @require 'connect.php';
 
 try {
-    $query = $pdo->query('SELECT * FROM clients');
+    $query = $pdo->query('SELECT firstname, lastname, birthDate, cardNumber FROM clients');
     $data = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
+    foreach ($data as $row) {
+        $fidelity = "No";
+        if (!empty($row['cardNumber'])) {
+            $fidelity = "Yes";
+        }
+        echo "Name: " . $row['lastname'] . "<br>";
+        echo "Firstname: " . $row['firstname'] . "<br>";
+        echo "Birthday: " . $row['birthDate'] . "<br>";
+        echo "Member: " . $fidelity . "<br>";
+        if ($fidelity == "Yes") {
+            echo "CardNumber :" . (string)$row['cardNumber'] . "<br>";
+        }
+        echo "<br>";
+    }
 } catch (Exception $e) {
     die('Erreur PDO: ' . $e->getMessage());
 }
 
 
 try {
-    $query = $pdo->query('SELECT type FROM showTypes');
+    $query = $pdo->query('SELECT * FROM shows ORDER BY title ASC');
     $data = $query->fetchAll(PDO::FETCH_ASSOC);
-    print_r($data);
+
+    echo "<h2>Shows</h2>";
+
+    foreach ($data as $row) {
+        echo "Spectacle :" . $row['title'] . "<br>";
+        echo "Artist :" . $row['performer'] . "<br>";
+        echo "Date :" . $row['date'] . "<br>";
+        echo "Start-Time :" . $row['startTime'] . "<br><br>";
+    }
 } catch (Exception $e) {
     die('Erreur PDO: ' . $e->getMessage());
 }
