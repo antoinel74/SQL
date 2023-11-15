9ae1078c00bc66e315e29da25f79192470ac59ba
antoinel74 <antoine.lansman@gmail.com>
2023-11-15 15:40:11 +0100

    add connect

---
+++ auth/connect.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=becode', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
